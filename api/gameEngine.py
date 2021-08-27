a4d5abdc770845a97dbd29273c72aace78d623a6
Muqing Bian <bianmuqing@gmail.com>
2021-08-28 01:37:47 +1000

    Init bingoGameEngine

---
+++ api/gameEngine.py
@@ -0,0 +1,24 @@
+import random
+
+class BingoGameEngine():
+    def _init_(self, playerNumber, callInterval, boardSize=5):
+        self.gameStatus = 0
+        self.playerNumber = playerNumber
+        self.interval = callInterval
+        self.boardSize = boardSize
+        self.players = {}
+
+    def registerPlayers(self):
+        pass
+    
+    def updateBoard(self, player, newBoard):
+        pass
+
+    def startGame(self):
+        pass
+
+    def nextNumber(self):
+        pass
+    
+    def endGame():
+        pass
