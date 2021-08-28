3a8517ae042aef69de5e7cbe47288508502c7892
Ben <PseudoRAM@users.noreply.github.com>
2021-08-29 06:27:22 +1000

    feat: number ball

--- src/applets/bingo/index.tsx
+++ src/applets/bingo/index.tsx
@@ -23,7 +23,6 @@ const joinGame = async () => {
   const userId = await axios.post(
     "https://api.clubspace.link/bingo/join",
     {
-      // prettier-ignore
       roomid: clubCodeFromPageUrl(),
     },
     {
@@ -33,22 +32,68 @@ const joinGame = async () => {
   return userId.data;
 };
 
+const checkGameStatus = async (userId: string) => {
+  const res = await axios.post(
+    "https://api.clubspace.link/bingo/gameinfo",
+
+    {
+      uuid: userId,
+    },
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  return res.data;
+};
+
+const triggerGameStart = async () => {
+  const res = await axios.post("https://api.clubspace.link/bingo/start");
+  return res.data;
+};
+
+const getCurrentNumber = async () => {
+  const res = await axios.post("https://api.clubspace.link/bingo/currnumber");
+  return res.data;
+};
+
 const Interface = ({ onClose, isOpen, onOpen }: IProps) => {
   const [gameStarted, setGameStarted] = useState(false);
   const [userId, setUserId] = useState<string>();
   const [numbers, setNumbers] = useState<number[][]>([]);
+  const [currentNumber, setCurrentNumber] = useState<number>();
 
   const fetchUser = async () => {
     const gameData = await joinGame();
-    setUserId(gameData.data.id);
+    setUserId(gameData.data.uuid);
     setNumbers(gameData.data.board);
+  };
+
+  const checkGame = async () => {
+    const data = await getCurrentNumber();
+    if (data.data.currNum !== -1 && !gameStarted) {
+      setGameStarted(true);
+    } else if (data.data.currNum === -1 && gameStarted) {
+      // TODO: display game over
+      setGameStarted(false);
+    }
+    if (data.data.currNum !== currentNumber) {
+      setCurrentNumber(parseInt(`${data.data.currNum}`));
+    }
   };
 
   useEffect(() => {
     if (!userId) {
       fetchUser();
     }
-  }, []);
+    const intervalId = window.setInterval(() => {
+      if (userId) {
+        checkGame();
+      }
+    }, 5000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [userId]);
 
   return (
     <>
@@ -76,7 +121,8 @@ const Interface = ({ onClose, isOpen, onOpen }: IProps) => {
           <button
             onClick={() => {
               onClose && onClose();
-              setGameStarted(true);
+              triggerGameStart();
+              // setGameStarted(true);
             }}
           >
             Start Game
@@ -94,6 +140,9 @@ const Interface = ({ onClose, isOpen, onOpen }: IProps) => {
           }}
           isOpen={isOpen}
         />
+      )}
+      {currentNumber && gameStarted && (
+        <DisplayNewNumber number={currentNumber} />
       )}
     </>
   );
@@ -160,6 +209,10 @@ const NumberDisplay = ({ number }: any) => {
       {number !== -1 ? number : "FREE"}
     </div>
   );
+};
+
+const DisplayNewNumber = ({ number }: any) => {
+  return <div className="number-display">{number}</div>;
 };
 
 const Applet = { id, title, icon, description, available, Interface };
