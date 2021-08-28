a488074771b660ffe5e50cbcafd5699532369342
Ricky Felix <rickyfelix19@outlook.com>
2021-08-28 13:40:00 +1000

    api comments

--- api/api.py
+++ api/api.py
@@ -7,3 +7,23 @@ app = Flask(__name__)
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+# list of API needed: 
+
+# GET 
+
+# 1) GET game status
+
+# 5) GET CurrBingoNum 
+
+# POST
+
+# 2) POST join game 
+
+# 3) POST start game
+
+# 4) POST win Bingo(recognized keyword Passing) 
+
+# 6) POST send bingo number and position 
+
+# DELETE
