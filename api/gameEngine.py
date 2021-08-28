cb39264148f26bb8af44d5ac9749bcaaf988ba62
Muqing Bian <bianmuqing@gmail.com>
2021-08-28 12:11:36 +1000

    Add basic functinos in BingoGameEngine

--- api/gameEngine.py
+++ api/gameEngine.py
@@ -1,4 +1,7 @@
 import random
+import asyncio
+
+
 
 class BingoGameEngine():
     def _init_(self, playerNumber, callInterval, boardSize=5):
@@ -15,10 +18,27 @@ class BingoGameEngine():
         pass
 
     def startGame(self):
-        pass
+        self.gameStatus = 1
+        self.runGame(self.interval)
+        return True
 
-    def nextNumber(self):
-        pass
+    def gameInfo(self):
+        gameInfo = {}
+        gameInfo['status'] = self.gameStatus
+        gameInfo['players'] = []
+        return gameInfo
+
+    def currNumber(self):
+        return self.currNumber
     
     def endGame():
-        pass
+        self.gameStatus = 0
+        return
+
+    async def runGame(self, interval):
+        while self.status != 0:
+            self.currNumber = random.randint(1,100)
+            self.gameStatus += 1
+            print(f"[Bingo] Turn{self.currNumber}: number is {self.currNumbernumber}, next number will reveal in {interval} second")
+            await asyncio.sleep(interval)
+        return
